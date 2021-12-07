190a20c19cb80ebb0cf44b40bb2d777f9aa739ff
Guillaume Guerin <guillaume.guerin@epitech.eu>
2021-12-07 16:16:51 +0100

    add Update / Create task

--- src/Views/Task.svelte
+++ src/Views/Task.svelte
@@ -6,6 +6,7 @@
 
     let user = {};
     let tasks = [];
+    let newtask = "";
 
     function disconnect() {
         localStorage.removeItem("access_token");
@@ -44,6 +45,7 @@
     }
 
     async function updateTask(taskId) {
+        console.log("Update Task")
         if (!$token) return disconnect();
 
         const task = tasks.find(e => e.id == taskId);
@@ -80,8 +82,61 @@
         getTasks();
     }
 
+    async function deleteTask(taskId) {
+        console.log("Delete task", taskId)
+        if (!$token) return disconnect();
+
+        const res = await fetch(`http://localhost:3000/task/${taskId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${$token}`
+            }
+        });
+
+        if (res.status != 200) {
+            return disconnect();
+        }
+
+        // Update tasks
+        getTasks();
+    }
+
+    async function createTask() {
+        if (!$token) return disconnect();
+
+        const body = {
+            title: newtask
+        }
+
+        const res = await fetch(`http://localhost:3000/task`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${$token}`
+            },
+            body: JSON.stringify(body)
+        });
+
+        if (res.status != 200) {
+            return disconnect();
+        }
+        newtask = "";
+
+        // Update tasks
+        getTasks();
+    }
+
+    async function handleKeyUp(event, taskId) {
+        if (event.keyCode == 13) {
+            // Enter key
+            console.log("Create or update task", taskId);
+            if (taskId) await updateTask(taskId);
+            else await createTask();
+        }
+    }
+
     onMount(async () => {
-        console.log("onMount")
         getTasks();
         getUser();
     })
@@ -89,21 +144,36 @@
 
 <main>
     <Navbar name={user.username}/>
-        {#each tasks as task (task.id)}
-            <div class="task-list">
-                <input type="checkbox" bind:checked={task.done} on:change={updateTask(task.id)} />
-                <p>{task.title}</p>
-            </div>
-        {/each}
+        <div class="task-list">
+            {#each tasks as task (task.id)}
+                <div class="task">
+                    <input type="checkbox" bind:checked={task.done} on:change={() => updateTask(task.id)} />
+                    <input bind:value={task.title} on:keyup={() => handleKeyUp(event, task.id)} />
+                    <button on:click={deleteTask(task.id)}>X</button>
+                </div>
+            {/each}
+            <input placeholder="Add a task" bind:value={newtask} on:keyup={() => handleKeyUp(event, null)} />
+        </div>
     <button on:click={disconnect}>
         Disconnect
     </button>
 </main>
 
 <style>
-    .task-list {
+    .task {
         display: flex;
         align-items: center;
         justify-content: center;
+    }
+
+    input {
+        border: none;
+        color: black;
+        background-color: var(--body-color);
+        /* width: 80%; */
+    }
+    input:focus {
+        outline: none;
+        border-bottom: 2px solid grey;
     }
 </style>
